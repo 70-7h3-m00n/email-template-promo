a4e82deb958371b87d2ef5d2df6df82b066e0cf6
Novailoveyou <novailoveyoux@gmail.com>
2021-08-26 22:03:43 +0300

    figured out negative margin alternative to fit in text on image

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -12,23 +12,12 @@ const Banner = () => {
     <Container alignment='center' style={stls.container}>
       <Row>
         <Column style={stls.column}>
-          <Image
+          {/* <Image
             src='https://res.cloudinary.com/mipinstitute/image/upload/v1629971752/temp%20ipo%20email/banner_tjogdh.jpg'
             width={600}
             height={300}
             alt='Банер'
-          />
-          {/* <h1 style={stls.title}>
-            Московский <br />
-            институт <br />
-            профессионального <br />
-            образования
-          </h1>
-          <Link href={'https://ipo.msk.ru/'} style={stls.btn}>
-            Перейти на сайт
-          </Link> */}
-        </Column>
-        <Column style={stls.column}>
+          /> */}
           <h1 style={stls.title}>
             Московский <br />
             институт <br />
@@ -39,6 +28,17 @@ const Banner = () => {
             Перейти на сайт
           </Link>
         </Column>
+        {/* <Column style={stls.column}>
+          <h1 style={stls.title}>
+            Московский <br />
+            институт <br />
+            профессионального <br />
+            образования
+          </h1>
+          <Link href={'https://ipo.msk.ru/'} style={stls.btn}>
+            Перейти на сайт
+          </Link>
+        </Column> */}
       </Row>
     </Container>
   )
